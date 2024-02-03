b7579004dd9705803339fe631bd37d0246362256
ireeus <34350042+ireeus@users.noreply.github.com>
2024-02-03 16:25:50 +0000

    errors corrected

--- config.php
+++ config.php
@@ -1,5 +1,17 @@
 <?php
-
+///////////////////////////////////////////////////////////
+////////////////Images upload /////////////////////////////
 $imgSize="2"; //Megabytes
 $imgQuality="10"; //% of the original quality 70% = 70
+
+////////////////Messages///////////////////////////////////
 $messageUpdate="1500"; //chat refresh interval in miliseconds
+
+/////////////////Error Reporting///////////////////////////
+//no errors
+error_reporting(0);
+ini_set('display_errors', 0);
+
+//show errors
+//error_reporting(E_ALL);
+//ini_set('display_errors', 1);
